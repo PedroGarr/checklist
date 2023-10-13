78ddf217b9e0946bad7520cdda825a2772e764e9
PsGarr <pes.garr@gmail.com>
2023-10-13 18:11:27 +0200

    Refined the UserModel database

--- src/main/java/pedrogarr/checklist/user/UserController.java
+++ src/main/java/pedrogarr/checklist/user/UserController.java
@@ -11,6 +11,6 @@ public class UserController {
     
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+        System.out.println(userModel.getName());
     }
 }
--- src/main/java/pedrogarr/checklist/user/UserModel.java
+++ src/main/java/pedrogarr/checklist/user/UserModel.java
@@ -1,34 +1,31 @@
 package pedrogarr.checklist.user;
 
-public class UserModel {
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+ //Lombok data is making the Setters and Getters
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+    
+    @Id //PRIMARY KEY
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+   
     private String username;
     private String name;
     private String password;
 
-    //Setters and Getters
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
     
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
