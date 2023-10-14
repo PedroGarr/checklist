13887fc116e5dbedd7779b3c20ab55a9e46b0fc7
PsGarr <pes.garr@gmail.com>
2023-10-14 15:20:59 +0200

    Hashed user password

--- src/main/java/pedrogarr/checklist/user/UserController.java
+++ src/main/java/pedrogarr/checklist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -23,6 +25,10 @@ public class UserController {
             
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This user already exists");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12,userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
